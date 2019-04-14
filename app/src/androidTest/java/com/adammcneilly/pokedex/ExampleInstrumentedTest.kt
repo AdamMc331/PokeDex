ffb09c9300dd0afd8b2588c9027d5a977c1fa674
adammc331 <amcneilly331@gmail.com>
2019-04-13 21:31:37 -0400

    Removed needless lines, also switched this back to one stage for convenience.

--- app/src/androidTest/java/com/adammcneilly/pokedex/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/adammcneilly/pokedex/ExampleInstrumentedTest.kt
@@ -7,7 +7,6 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
 /**
  * Instrumented test, which will execute on an Android device.
  *
--- app/src/test/java/com/adammcneilly/pokedex/ExampleUnitTest.kt
+++ app/src/test/java/com/adammcneilly/pokedex/ExampleUnitTest.kt
@@ -3,7 +3,6 @@ package com.adammcneilly.pokedex
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
