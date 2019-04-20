4ad2c9f50a725cda3a898d683d79682c3d163f27
adammc331 <amcneilly331@gmail.com>
2019-04-20 13:30:19 -0400

    Fixed detekt warnings.

--- app/src/main/java/com/adammcneilly/pokedex/BindingAdapters.kt
+++ app/src/main/java/com/adammcneilly/pokedex/BindingAdapters.kt
@@ -9,6 +9,9 @@ import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 
+private const val PROGRESS_STROKE_WIDTH = 10F
+private const val PROGRESS_CENTER_RADIUS = 30F
+
 @BindingAdapter("visibilityCondition")
 fun View.visibleIf(condition: Boolean?) {
     this.visibility = if (condition == true) View.VISIBLE else View.GONE
@@ -28,10 +31,12 @@ fun Toolbar.textColorRes(colorRes: Int?) {
 
 @BindingAdapter("imageUrl")
 fun ImageView.imageUrl(imageUrl: String) {
+    val progressColor = ContextCompat.getColor(context, R.color.colorAccent)
+
     val circularProgressDrawable = CircularProgressDrawable(this.context)
-    circularProgressDrawable.strokeWidth = 10f
-    circularProgressDrawable.centerRadius = 30f
-    circularProgressDrawable.setColorFilter(ContextCompat.getColor(context, R.color.colorAccent), PorterDuff.Mode.SRC_IN)
+    circularProgressDrawable.strokeWidth = PROGRESS_STROKE_WIDTH
+    circularProgressDrawable.centerRadius = PROGRESS_CENTER_RADIUS
+    circularProgressDrawable.setColorFilter(progressColor, PorterDuff.Mode.SRC_IN)
     circularProgressDrawable.start()
 
     Glide.with(this)
