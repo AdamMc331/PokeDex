a239dbb49dea18ce41e4833840d1feea07d1840f
adammc331 <amcneilly331@gmail.com>
2019-04-21 10:15:16 -0400

    Fixed type tests.

--- app/src/test/java/com/adammcneilly/pokedex/detail/DetailActivityViewModelTest.kt
+++ app/src/test/java/com/adammcneilly/pokedex/detail/DetailActivityViewModelTest.kt
@@ -2,18 +2,14 @@ package com.adammcneilly.pokedex.detail
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.adammcneilly.pokedex.R
-import com.adammcneilly.pokedex.models.Color
-import com.adammcneilly.pokedex.models.Pokemon
-import com.adammcneilly.pokedex.models.Species
+import com.adammcneilly.pokedex.models.*
 import com.adammcneilly.pokedex.network.PokemonAPI
 import com.adammcneilly.pokedex.network.PokemonRepository
 import com.adammcneilly.pokedex.whenever
 import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import org.junit.Assert.assertTrue
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.ArgumentMatchers.anyString
@@ -34,7 +30,7 @@ class DetailActivityViewModelTest {
 
     @Test
     fun loadData() {
-        val testPokemon = Pokemon(name = "Adam")
+        val testPokemon = Pokemon(name = "Adam", types = listOf(TypeSlot(type = Type("grass"))))
         val testSpecies = Species(color = Color("green"))
 
         whenever(mockAPI.getPokemonByName(anyString())).thenReturn(Single.just(testPokemon))
@@ -45,8 +41,55 @@ class DetailActivityViewModelTest {
         assertTrue(viewModel.showData)
         assertFalse(viewModel.showError)
         assertEquals(testPokemon.name?.capitalize(), viewModel.title)
-        assertEquals(R.color.mds_green_500, viewModel.toolbarColorRes)
+        assertEquals(R.color.type_grass, viewModel.toolbarColorRes)
         assertEquals(R.color.mds_white, viewModel.toolbarTextColorRes)
+    }
+
+    @Test
+    fun getTypesWithNoTypes() {
+        whenever(mockAPI.getPokemonByName(anyString())).thenReturn(Single.just(Pokemon()))
+        whenever(mockAPI.getPokemonSpecies(anyString())).thenReturn(Single.just(Species()))
+        val viewModel = DetailActivityViewModel(repository, "")
+
+        assertNull(viewModel.firstType)
+        assertNull(viewModel.secondType)
+        assertFalse(viewModel.showFirstType)
+        assertFalse(viewModel.showSecondType)
+    }
+
+    @Test
+    fun getTypesWithOneType() {
+        val firstType = Type("grass", "grassurl")
+        val testPokemon = Pokemon(
+            types = listOf(TypeSlot(slot = 1, type = firstType))
+        )
+
+        whenever(mockAPI.getPokemonByName(anyString())).thenReturn(Single.just(testPokemon))
+        whenever(mockAPI.getPokemonSpecies(anyString())).thenReturn(Single.just(Species()))
+        val viewModel = DetailActivityViewModel(repository, testPokemon.name.orEmpty())
+
+        assertEquals(firstType, viewModel.firstType)
+        assertNull(viewModel.secondType)
+        assertTrue(viewModel.showFirstType)
+        assertFalse(viewModel.showSecondType)
+    }
+
+    @Test
+    fun getTypesWithTwoTypes() {
+        val firstType = Type("grass", "grassurl")
+        val secondType = Type("bug", "bugurl")
+        val testPokemon = Pokemon(
+            types = listOf(TypeSlot(slot = 1, type = firstType), TypeSlot(slot = 2, type = secondType))
+        )
+
+        whenever(mockAPI.getPokemonByName(anyString())).thenReturn(Single.just(testPokemon))
+        whenever(mockAPI.getPokemonSpecies(anyString())).thenReturn(Single.just(Species()))
+        val viewModel = DetailActivityViewModel(repository, testPokemon.name.orEmpty())
+
+        assertEquals(firstType, viewModel.firstType)
+        assertEquals(secondType, viewModel.secondType)
+        assertTrue(viewModel.showFirstType)
+        assertTrue(viewModel.showSecondType)
     }
 
     @Test
