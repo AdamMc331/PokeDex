6c9cbbd5d0d7cc374ef2a2431ff4d9f7af3f4d03
adammc331 <amcneilly331@gmail.com>
2019-05-18 17:50:28 -0400

    Fixed lint issue.

--- app/src/main/java/com/adammcneilly/pokedex/network/PokemonRetrofitService.kt
+++ app/src/main/java/com/adammcneilly/pokedex/network/PokemonRetrofitService.kt
@@ -3,7 +3,7 @@ package com.adammcneilly.pokedex.network
 import com.adammcneilly.pokedex.models.Pokemon
 import com.adammcneilly.pokedex.models.PokemonResponse
 
-open class PokemonRetrofitService(private val api: PokemonAPI): PokemonRepository {
+open class PokemonRetrofitService(private val api: PokemonAPI) : PokemonRepository {
     override suspend fun getPokemon(): PokemonResponse {
         return api.getPokemonAsync().await()
     }
