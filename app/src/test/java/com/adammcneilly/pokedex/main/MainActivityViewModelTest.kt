82afce7f569c9c1336b09913a4e18fe242e27001
adammc331 <amcneilly331@gmail.com>
2019-06-16 17:47:37 -0400

    Lint fixes.

--- app/src/test/java/com/adammcneilly/pokedex/main/MainActivityViewModelTest.kt
+++ app/src/test/java/com/adammcneilly/pokedex/main/MainActivityViewModelTest.kt
@@ -1,20 +1,10 @@
 package com.adammcneilly.pokedex.main
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.adammcneilly.pokedex.DispatcherProvider
 import com.adammcneilly.pokedex.models.Pokemon
 import com.adammcneilly.pokedex.models.PokemonResponse
-import com.adammcneilly.pokedex.network.PokemonRepository
-import com.adammcneilly.pokedex.testObserver
-import com.adammcneilly.pokedex.whenever
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito.mock
 
 @Suppress("UNCHECKED_CAST")
 class MainActivityViewModelTest {
