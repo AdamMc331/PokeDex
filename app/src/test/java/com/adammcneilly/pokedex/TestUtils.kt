b06c391c96cdad159b705df3dc6d0f7641a06fa0
adammc331 <amcneilly331@gmail.com>
2019-09-13 23:34:01 -0400

    Unit tested PokemonService.

--- app/src/test/java/com/adammcneilly/pokedex/TestUtils.kt
+++ app/src/test/java/com/adammcneilly/pokedex/TestUtils.kt
@@ -2,10 +2,15 @@ package com.adammcneilly.pokedex
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import org.mockito.Mockito
 import org.mockito.stubbing.OngoingStubbing
 
 fun <T> whenever(methodCall: T): OngoingStubbing<T> = Mockito.`when`(methodCall)
+
+@Suppress("DeferredIsResult")
+fun <T> T.toDeferred() = GlobalScope.async { this@toDeferred }
 
 class TestObserver<T> : Observer<T> {
     var observedValue: T? = null
