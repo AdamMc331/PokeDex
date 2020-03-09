1b56f65d6b6ccb62a00d46e7cf7490230e50bbb7
adammc331 <amcneilly331@gmail.com>
2020-03-08 22:24:39 -0400

    Navigating backwards using back button.

--- app/src/main/java/com/adammcneilly/pokedex/main/MainActivity.kt
+++ app/src/main/java/com/adammcneilly/pokedex/main/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.adammcneilly.pokedex.main
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.adammcneilly.pokedex.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        setupActionBarWithNavController(
+            navController = findNavController(R.id.nav_host_fragment)
+        )
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> findNavController(R.id.nav_host_fragment).navigateUp()
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
