e862614b84889788a5a6aa896698356d86b2a197
Adam McNeilly <amcneilly331@gmail.com>
2020-04-21 21:58:25 -0400

    Updating dependencies for Danger. (#77)
    * Updating dependencies for Danger.
    
    * Updating a bunch of dependencies.
    
    * Targeting JVM 1.8

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -1,6 +1,6 @@
 object Config {
     const val compileSdk = 29
-    const val minSdk = 16
+    const val minSdk = 21
     const val targetSdk = 29
     const val appVersionCode = 1
     const val appVersionName = "1.0.0"
