f0f871dd681cfa1340e3f59d1c9154db3984923e
adammc331 <amcneilly331@gmail.com>
2020-05-16 19:11:36 -0400

    Adding image to list items, fetching it from the server using a hardcode.

--- app/src/test/java/com/adammcneilly/pokedex/viewmodels/PokemonViewModelTest.kt
+++ app/src/test/java/com/adammcneilly/pokedex/viewmodels/PokemonViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.adammcneilly.pokedex.viewmodels
 
+import com.adammcneilly.pokedex.core.Pokemon
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -9,5 +10,35 @@ class PokemonViewModelTest {
     fun getNameNoPokemon() {
         val viewModel = PokemonViewModel()
         assertEquals("", viewModel.name)
+    }
+
+    @Test
+    fun getImageUrlNoPokemon() {
+        val viewModel = PokemonViewModel()
+        assertEquals(null, viewModel.imageUrl)
+    }
+
+    @Test
+    fun getNameWithPokemon() {
+        val viewModel = PokemonViewModel()
+        val pokemon = Pokemon(name = "Adam")
+        viewModel.pokemon = pokemon
+        assertEquals("Adam", viewModel.name)
+    }
+
+    @Test
+    fun useExistingImageURLIfSupplied() {
+        val viewModel = PokemonViewModel()
+        val pokemon = Pokemon(frontSpriteUrl = "Adam")
+        viewModel.pokemon = pokemon
+        assertEquals("Adam", viewModel.imageUrl)
+    }
+
+    @Test
+    fun formatNewUrlWithPokedexNumber() {
+        val viewModel = PokemonViewModel()
+        val pokemon = Pokemon(pokedexNumber = "1")
+        viewModel.pokemon = pokemon
+        assertEquals("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png", viewModel.imageUrl)
     }
 }
