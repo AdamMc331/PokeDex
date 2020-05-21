d022ecbad26ea8a1b2a39c2704ad325faf55b786
Adam McNeilly <amcneilly331@gmail.com>
2020-05-20 20:22:27 -0400

    Adding the pokemon photo to the list items.  (#91)
    * Adding image to list items, fetching it from the server using a hardcode.

--- app/src/main/java/com/adammcneilly/pokedex/viewmodels/PokemonViewModel.kt
+++ app/src/main/java/com/adammcneilly/pokedex/viewmodels/PokemonViewModel.kt
@@ -14,4 +14,28 @@ class PokemonViewModel : BaseObservable() {
     val name: String
         @SuppressLint("DefaultLocale")
         get() = pokemon?.name?.capitalize().orEmpty()
+
+    val imageUrl: String?
+        get() {
+            val currentUrl = pokemon?.frontSpriteUrl.orEmpty()
+
+            return if (currentUrl.isNotEmpty()) {
+                currentUrl
+            } else {
+                pokemon?.pokedexNumber?.let { pokedexNumber ->
+                    POKE_API_SPRITE_FORMAT.format(pokedexNumber)
+                }
+            }
+        }
+
+    companion object {
+        /**
+         * We use the open source PokeApi.co to power this app, but unfortunately their pokemon list
+         * endpoint does not supply sprites with it. However, each sprite follows the same format,
+         * so we can just use this string and format it with the corresponding pokedex number
+         * if we don't have an imageUrl.
+         */
+        private const val POKE_API_SPRITE_FORMAT =
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/%s.png"
+    }
 }
--- app/src/test/java/com/adammcneilly/pokedex/viewmodels/PokemonViewModelTest.kt
+++ app/src/test/java/com/adammcneilly/pokedex/viewmodels/PokemonViewModelTest.kt
@@ -1,13 +1,59 @@
 package com.adammcneilly.pokedex.viewmodels
 
-import org.junit.Assert.assertEquals
+import com.adammcneilly.pokedex.core.Pokemon
+import org.junit.Before
 import org.junit.Test
 
 class PokemonViewModelTest {
+    private lateinit var testRobot: PokemonViewModelRobot
+
+    @Before
+    fun setUp() {
+        testRobot = PokemonViewModelRobot()
+    }
 
     @Test
     fun getNameNoPokemon() {
-        val viewModel = PokemonViewModel()
-        assertEquals("", viewModel.name)
+        testRobot
+            .setPokemon(null)
+            .assertPokemonName("")
+    }
+
+    @Test
+    fun getImageUrlNoPokemon() {
+        testRobot
+            .setPokemon(null)
+            .assertImageUrl(null)
+    }
+
+    @Test
+    fun getNameWithPokemon() {
+        val pokemonName = "Adam"
+        val pokemon = Pokemon(name = pokemonName)
+
+        testRobot
+            .setPokemon(pokemon)
+            .assertPokemonName(pokemonName)
+    }
+
+    @Test
+    fun useExistingImageURLIfSupplied() {
+        val frontImage = "Adam"
+        val pokemon = Pokemon(frontSpriteUrl = frontImage)
+
+        testRobot
+            .setPokemon(pokemon)
+            .assertImageUrl(frontImage)
+    }
+
+    @Test
+    fun formatNewUrlWithPokedexNumber() {
+        val pokemon = Pokemon(pokedexNumber = "1")
+
+        testRobot
+            .setPokemon(pokemon)
+            .assertImageUrl(
+                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+            )
     }
 }
