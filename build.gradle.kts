9d553a6a92e64bcce57dc399030e46e9925336fc
adammc331 <amcneilly331@gmail.com>
2020-07-03 01:56:15 -0400

    Some version updates.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0-alpha02")
+        classpath("com.android.tools.build:gradle:4.2.0-alpha03")
         classpath(kotlin("gradle-plugin", version = Versions.kotlin))
         classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:${Versions.detekt}")
         classpath("org.jlleitschuh.gradle:ktlint-gradle:${Versions.ktlint}")
